8abe4e74296926083cd9287471976aed3fe5ee4f
Pavel Machuca <pavelmachuca@hotmail.com>
2019-07-15 20:37:40 -0400

    passing again

--- calculator.test.js
+++ calculator.test.js
@@ -7,7 +7,7 @@ test('addition works', function () {
 })
 
 test('addition works, strings converted to numbers', function () {
-  expect(functions.calculator('addition', '1', '2')).toBe('dog');
+  expect(functions.calculator('addition', '1', '2')).toBe(3);
 })
 
 test('subtraction works', function () {
